4fa12628aad7a7f1ef7330ad7962c668a51995e3
kmartinss <martinskazk@gmail.com>
2024-11-07 00:21:29 -0300

    first commit

---
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -0,0 +1,47 @@
+from typing import List
+from fastapi import APIRouter
+from pydantic import BaseModel
+from decimal import Decimal
+
+router = APIRouter(prefix="/contas-a-pagar-e-receber")
+
+
+class ContaPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str  # PAGAR E RECEBER
+
+
+class ContaPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str  # PAGAR E RECEBER
+
+
+@router.get("/", response_model=List[ContaPagarReceberResponse])
+def listar_contas():
+    return [
+       ContaPagarReceberResponse(
+           id=1,
+           descricao="aluguel",
+           valor=1000.50,
+           tipo="PAGAR"
+       ),
+       ContaPagarReceberResponse(
+           id=1,
+           descricao="salário",
+           valor=5000,
+           tipo="receber"
+        ),
+    ]
+
+
+@router.post("/", response_model=ContaPagarReceberResponse, status_code=201)
+def criar_conta(conta: ContaPagarReceberRequest):
+    return ContaPagarReceberResponse(
+        id=3,
+        descricao=conta.descricao,
+        valor=conta.valor,
+        tipo=conta.tipo
+    )
---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
+
+app = FastAPI()
+
+
+@app.get("/")
+def oi_eu_sou_programador():
+    return "oi eu sou programador"
+
+
+app.include_router(contas_a_pagar_e_receber_router.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
